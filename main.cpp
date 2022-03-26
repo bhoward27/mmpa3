76d29d155bd5c541aedcd066a0d4201da6b9ae6c
Benjamin Howard <bentlife@icloud.com>
2022-03-26 16:17:25 -0700

    Add key files (main.cpp, Makefile, and .gitignore).

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+    Assignment Description:
+
+    2D (Discrete Cosine Transform) 
+    
+    Read N (an integer between 2 and 16) and then an input matrix of size NxN from an input file.
+    Each matrix component is an integer between 0 and 255. Print out the 2D DCT transform result (rounding to integers) with
+        (1) rows transform first and then columns, and
+        (2) columns transform first and then rows.
+    
+    Note that you can't call any lib to do this; you have to write your own code to implement the matrix operation.
+    Discuss whether results (1) and (2) are identical. If not, what's the potential problems.
+*/
+
+#include <iostream>
+#include <array>
+
+using std::cin;
+using std::cout;
+using std::array;
+
+int main() {
+    
+
+    return 0;
+}
